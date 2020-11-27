c070d2f6dd280aaaa07690ac9de3ec9bac8709ec
JohanTv <johan.tanta@utec.edu.pe>
2020-11-27 18:13:26 -0500

    saved.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import io
 from random import randint as rand
-def random_writes: # TODO
-    with open('output.txt2','w') as file:
-#        for i in range(1000000000):
-        for i in range(1000**2):
-            file.seek(rand(0,500000000,0)
-            file.write('HOLA'*1000000)
-def random_reads:
-    with open('output.txt2','r') as file:
-        for i in range(1000**2):
-            file.seek(rand(0,500000000,0)
-            file.read(int(1024**3))
 
-def seq_writes:
-    with open('output.txt2','w') as file:
-        #for i in range(1000000000):
-        for i in range(1000):
-            file.write('HOLA'*1000000)
-def seq_reads:
-    with open('output.txt2','r') as file:
-        for i in range(1000):
-            file.seek(0,0)
-            file.write('HOLA'*1000000)
-            
-#codigo para el iostat: iostat -x 1 
-#ejecutar coddigo de python: python3 main.py
+name = 'output.txt2'
+
+def random_writes():
+    f = open(name, 'w')
+    for i in range(1000**2):
+        f.seek(rand(0, 500000000, 0))
+        f.write('HOLA'*1000000)
+    f.close()
+
+def random_reads():
+    f=open(name, 'r')
+    for i in range(1000**2):
+        f.seek(rand(0, 500000000, 0))
+        f.read(int(1024**3))
+    f.close()
+
+def seq_writes():
+    f=open(name, 'w')
+    for i in range(1000):
+        f.write('HOLA'*1000000)
+    f.close()
+
+def seq_reads():
+    f=open(name, 'r')
+    for i in range(1000):
+        f.seek(0, 0)
+        f.write('HOLA'*1000000)
+    f.close()
+
+# codigo para el iostat: iostat -x 1
+# ejecutar coddigo de python: python3 main.py
