e0a2a8f213837f6e9fb3fdc34184dccb75b14609
cesar <camgcamg11@gmail.com>
2020-11-29 01:12:30 -0500

    fixed some status

--- main.py
+++ main.py
@@ -4,31 +4,39 @@ from random import randint as rand
 name = 'output.txt2'
 
 def random_writes():
+    to_write ='HOLA'*1000000000 
     f = open(name, 'w')
     for i in range(1000**2):
-        f.seek(rand(0, 500000000, 0))
-        f.write('HOLA'*1000000)
+        f.seek(rand(0, 5000000), 0)
+        f.write(to_write)
     f.close()
 
 def random_reads():
     f=open(name, 'r')
     for i in range(1000**2):
-        f.seek(rand(0, 500000000, 0))
+        f.seek(rand(0, 5000000), 0)
         f.read(int(1024**3))
     f.close()
 
 def seq_writes():
+    to_write ='HOLA'*1000000
     f=open(name, 'w')
     for i in range(1000):
-        f.write('HOLA'*1000000)
+        f.write(to_write)
     f.close()
 
 def seq_reads():
     f=open(name, 'r')
-    for i in range(1000):
-        f.seek(0, 0)
-        f.write('HOLA'*1000000)
+    value = int(1000**3)
+    for i in range(4):
+        #f.seek(0, 0)
+        f.read(value)
     f.close()
+
+#seq_writes()
+seq_reads()
+#random_writes()
+#random_reads()
 
 # codigo para el iostat: iostat -x 1
 # ejecutar coddigo de python: python3 main.py
