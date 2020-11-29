29d0dd1d532fefacce1467db66589bb181a115c3
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-11-29 17:38:25 -0500

    All txts produced

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import io
 from random import randint as rand
 
-name = 'output.txt2'
+name = 'output.txt'
 
 def random_writes():
     to_write ='HOLA'*1000000000 
     f = open(name, 'w')
-    for i in range(1000**2):
+    for i in range(10):
         f.seek(rand(0, 5000000), 0)
         f.write(to_write)
     f.close()
 
 def random_reads():
     f=open(name, 'r')
-    for i in range(1000**2):
-        f.seek(rand(0, 5000000), 0)
+    for i in range(100):
+        f.seek(rand(0, 5000), 0)
         f.read(int(1024**3))
     f.close()
 
@@ -34,9 +34,9 @@ def seq_reads():
     f.close()
 
 #seq_writes()
-seq_reads()
+#seq_reads()
 #random_writes()
-#random_reads()
+random_reads()
 
 # codigo para el iostat: iostat -x 1
 # ejecutar coddigo de python: python3 main.py
